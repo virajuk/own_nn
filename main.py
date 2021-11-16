1689a799058bd94d4ae9f5b0fcab21e75837a5fe
virajuk <virajudayakumara@gmail.com>
2021-11-16 12:48:32 -0500

    sigmoid function

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello_world")
+import math
+
+
+def sigmoid_function(x):
+
+    """
+    y = 1(1 + e^(-x))
+    :param x:
+    :return: y
+    """
+    y = 1/(1+math.exp(-x))
+    return y
+
+
+value = sigmoid_function(0.6)
+print(255*255*255*255)
+
