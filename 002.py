f992d30e9bd5df68dea5113cbdf0c86b69afea40
virajuk <virajudayakumara@gmail.com>
2021-11-29 09:47:02 -0500

    adding sigmoid function

--- 002.py
+++ 002.py
@@ -1,4 +1,5 @@
 from src.first import First
+from src.activation_function import Activation
 
 data = [[3, 1, 0], [1, 3, 1], [3.1, 0.975, 0], [3.02, 0.99, 0], [1.03, 3.04, 1], [2.97, 0.99, 0], [1.09, 3.02, 1], [2.99, 1.01, 0], [1.1, 3.14, 1], [1.07, 2.97, 1],
         [3.07, 0.91, 0], [0.97, 2.94, 1], [3.01, 0.91, 0], [2.9, 0.94, 0], [3.1, 1.08, 0], [0.97, 2.99, 1], [3.11, 1.15, 0], [2.88, 0.89, 0], [0.93, 2.89, 1], [0.95, 2.92, 1]]
@@ -14,10 +15,14 @@ for point in data:
 
     count[label] += 1
 
-first = First()
-first.split_train_eval_data(data)
+# first = First()
+# first.split_train_eval_data(data)
 
 # first.first_sample()
 # first.second_sample()
 
-first.train_model()
+# first.train_model()
+
+activation = Activation()
+threshold = activation.sigmoid_function(2)
+print(threshold)
