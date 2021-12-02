a404aae9480e0e24fa12a67977ab3b465424068c
virajuk <virajudayakumara@gmail.com>
2021-12-01 22:04:34 -0500

    moving exp files to dedicated directory

--- utils/utils.py
+++ utils/utils.py
@@ -3,10 +3,12 @@ import numpy as np
 
 
 def sigmoid_function(x):
+
     """
-        y = 1(1 + e^(-x))
-        :param x:
-        :return: y
-        """
+    y = 1(1 + e^(-x))
+    :param x:
+    :return: y
+    """
+
     y = 1 / (1 + math.exp(-x))
     return y
