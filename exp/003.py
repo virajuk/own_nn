495682ba1be70b29460fdf33482baa9de17da2b5
virajuk <virajudayakumara@gmail.com>
2021-12-01 22:31:42 -0500

    experimenting with partial differentials with sympy

--- exp/003.py
+++ exp/003.py
@@ -1,7 +1,16 @@
 import sympy as sp
+import math
 
-a, b, c = sp.symbols('a b c', real=True)
-f = 5*a*b - a*sp.cos(c) + a**2 + c**8*b
+###############################################################
+# a, b, l = sp.symbols('a b lambda', real=True)
+# f = 5 * a * b - a * sp.cos(l) + a ** 2 + l ** 8 * b
+#
+# # differentiating function f in respect to a
+# print(sp.diff(f, l))
+###############################################################
 
-# differentiating function f in respect to a
-print(sp.diff(f, a))
+
+tk, ok = sp.symbols('tk ok', real=True)
+
+e = (tk - ok)**2
+print(sp.diff(e, ok))
