3dca801935c606d4307fcfbd8d8823555b94d442
virajuk <virajudayakumara@gmail.com>
2021-12-02 11:13:19 -0500

    diferential equation for sigmoid function

--- exp/003.py
+++ exp/003.py
@@ -1,5 +1,7 @@
 import sympy as sp
-import math
+import numpy as np
+
+# import utils.utils as ut
 
 ###############################################################
 # a, b, l = sp.symbols('a b lambda', real=True)
@@ -9,8 +11,20 @@ import math
 # print(sp.diff(f, l))
 ###############################################################
 
+# ut.sigmoid_function()
 
-tk, ok = sp.symbols('tk ok', real=True)
+# tk, ok = sp.symbols('tk ok', real=True)
+#
+# e = (tk - ok)**2
+# print(sp.diff(e, ok))
 
-e = (tk - ok)**2
-print(sp.diff(e, ok))
+###############################################################
+
+x, y = sp.symbols('x y', real=True)
+
+# print(np.power(2, x))
+# fun = 1/(1 + math.pow(2,x))
+
+fun = 1/(1+sp.exp(-x))
+
+print(sp.diff(fun, x))
