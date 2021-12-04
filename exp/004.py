8ff0222360d489ed60b9b807a04b64461844974a
virajuk <virajudayakumara@gmail.com>
2021-12-04 10:41:07 -0500

    building first nn

--- exp/004.py
+++ exp/004.py
@@ -1,6 +1,11 @@
 import sympy as sp
 
-x, y = sp.symbols('x y', real=True)
-u = sp.asin(x/y)
+# x, y = sp.symbols('x y', real=True)
+# u = sp.asin(x/y)
+#
+# print(sp.diff(u, x))
 
-print(sp.diff(u, x))
+x, y = sp.symbols('x y', real=True)
+func = sp.sin(y)
+
+print(sp.diff(func, x))
---
+++ exp/005.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+input_matrix = np.array([[0.9], [0.1], [0.8]])
+
+weight_input_hidden = np.array([[0.9, 0.3, 0.4], [0.2, 0.8, 0.2], [0.1, 0.5, 0.6]])
+
+weight_hidden_output = np.array([[0.3, 0.7, 0.5], [0.6, 0.5, 0.2], [0.8, 0.1, 0.9]])
+
+print(input_matrix)
+print(weight_input_hidden)
+print(weight_hidden_output)
