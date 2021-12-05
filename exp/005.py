25e1671d65bd7efce6bade46f1cc28bfcf0b3cd3
virajuk <virajudayakumara@gmail.com>
2021-12-05 01:59:00 -0500

    adding PYTHONPATH to a shell script

--- exp/005.py
+++ exp/005.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from utils.utils import sigmoid_function
+from utils import utils as ut
 
 input_matrix = np.array([[0.9], [0.1], [0.8]])
 
@@ -8,18 +8,17 @@ weight_input_hidden = np.array([[0.9, 0.3, 0.4], [0.2, 0.8, 0.2], [0.1, 0.5, 0.6
 
 weight_hidden_output = np.array([[0.3, 0.7, 0.5], [0.6, 0.5, 0.2], [0.8, 0.1, 0.9]])
 
-# print(input_matrix)
-# print(weight_input_hidden)
-# print(weight_hidden_output)
-
 x_hidden = np.matmul(weight_input_hidden, input_matrix)
-print(x_hidden)
+# print(x_hidden)
 
-o_hidden = sigmoid_function(x_hidden)
-print(o_hidden)
+o_hidden = ut.sigmoid_function(x_hidden)
+# print(o_hidden)
 
 x_output = np.matmul(weight_hidden_output, o_hidden)
-print(x_output)
+# print(x_output)
 
-output_matrix = sigmoid_function(x_output)
+output_matrix = ut.sigmoid_function(x_output)
 print(output_matrix)
+
+error_matrix = np.array([[1.5], [0.5], [0.3]])
+
