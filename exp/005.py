595690be6a4f95d3a1a6ccbd8e5ee02e38447283
virajuk <virajudayakumara@gmail.com>
2021-12-05 13:08:06 -0500

    updating weight matrix

--- exp/005.py
+++ exp/005.py
@@ -5,11 +5,13 @@ from utils import utils as ut
 input_matrix = np.array([[0.9], [0.1], [0.8]])
 
 weight_input_hidden = np.array([[0.9, 0.3, 0.4], [0.2, 0.8, 0.2], [0.1, 0.5, 0.6]])
-
 weight_hidden_output = np.array([[0.3, 0.7, 0.5], [0.6, 0.5, 0.2], [0.8, 0.1, 0.9]])
+# print(weight_hidden_output.shape)
 
 x_hidden = np.matmul(weight_input_hidden, input_matrix)
 # print(x_hidden)
+
+# print(weight_input_hidden*input_matrix)
 
 o_hidden = ut.sigmoid_function(x_hidden)
 # print(o_hidden)
@@ -20,14 +22,23 @@ x_output = np.matmul(weight_hidden_output, o_hidden)
 output_matrix = ut.sigmoid_function(x_output)
 # print(output_matrix)
 
-error_matrix = np.array([[1.5], [0.5], [0.3]])
-print(error_matrix)
-
-# print(weight_hidden_output)
-# print(weight_hidden_output.T)
+error_matrix = np.array([[0.8], [0.5], [0.3]])
+# print(error_matrix)
 
 y_hidden = np.matmul(weight_hidden_output.T, error_matrix)
-print(y_hidden)
+# print(y_hidden)
 
 y_input = np.matmul(weight_input_hidden.T, y_hidden)
-print(y_input)
+# print(y_input)
+
+#######################################################################################
+## derivative of error
+
+delta_weight_hidden_output = np.matmul(-2*error_matrix*(ut.sigmoid_function(x_output))*(1-ut.sigmoid_function(x_output)), o_hidden.T)
+
+print(weight_hidden_output)
+print(delta_weight_hidden_output)
+
+updated_weight_hidden_output = weight_hidden_output + delta_weight_hidden_output
+print(updated_weight_hidden_output)
+
