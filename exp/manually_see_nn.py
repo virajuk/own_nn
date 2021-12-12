bb37ece59a922ed749196d3675e754e4e7346b6b
virajuk <virajudayakumara@gmail.com>
2021-12-12 03:23:29 -0500

    code refactoring

--- exp/manually_see_nn.py
+++ exp/manually_see_nn.py
@@ -10,42 +10,59 @@ weight_hidden_output = np.array([[1.0, 0.7], [1.6, 4.3]])
 
 LEARNING_RATE = 0.1
 
-# print(weight_hidden_output)
-# print(weight_hidden_output.shape)
 
-input_hidden = np.matmul(weight_input_hidden, input_input)
-# print(input_hidden)
+def single_iteration():
 
-output_hidden = ut.sigmoid_function(input_hidden)
-# print(output_hidden)
+    print(input_input)
 
-input_output = np.matmul(weight_hidden_output, output_hidden)
-# print(input_output)
+    # input & output of hidden layer
+    input_hidden = np.matmul(weight_input_hidden, input_input)
+    output_hidden = ut.sigmoid_function(input_hidden)
 
-output_output = ut.sigmoid_function(input_output)
-# print(output_output)
+    # input & output of output layer
+    input_output = np.matmul(weight_hidden_output, output_hidden)
 
-label = np.array([[1.0], [0.0]])
-error_output = (label - output_output)**2
-# print(error_output)
+single_iteration()
 
 
-#######################################################################################
-error_hidden = np.matmul(weight_hidden_output.T, error_output)
-# print(error_hidden)
 
-delta_weight_hidden_output = np.matmul(LEARNING_RATE*(-2)*error_output*(ut.sigmoid_function(input_output))*(1-ut.sigmoid_function(input_output)), output_hidden.T)
-print(delta_weight_hidden_output)
 
-# print(weight_hidden_output)
-new_weight_hidden_output = weight_hidden_output + delta_weight_hidden_output
-print(new_weight_hidden_output)
+# input_hidden = np.matmul(weight_input_hidden, input_input)
+# # print(input_hidden)
+#
+# output_hidden = ut.sigmoid_function(input_hidden)
+# # print(output_hidden)
+#
+# input_output = np.matmul(weight_hidden_output, output_hidden)
+# # print(input_output)
+#
+# output_output = ut.sigmoid_function(input_output)
+# # print(output_output)
+#
+# label = np.array([[1.0], [0.0]])
+# error_output = (label - output_output)**2
+# # print(error_output)
+#
+#
+# #######################################################################################
+# error_hidden = np.matmul(weight_hidden_output.T, error_output)
+# # print(error_hidden)
+#
+# delta_weight_hidden_output = np.matmul(LEARNING_RATE*(-2)*error_output*(ut.sigmoid_function(input_output))*(1-ut.sigmoid_function(input_output)), output_hidden.T)
+# print(delta_weight_hidden_output)
+#
+# # print(weight_hidden_output)
+# new_weight_hidden_output = weight_hidden_output + delta_weight_hidden_output
+# print(new_weight_hidden_output)
+#
+# error_input = np.matmul(weight_input_hidden.T, error_hidden)
+# # print(error_input)
+#
+# delta_weight_input_hidden = np.matmul(LEARNING_RATE*(-2)*error_hidden*(ut.sigmoid_function(input_hidden))*(1-ut.sigmoid_function(input_hidden)), input_hidden.T)
+# print(delta_weight_input_hidden)
+#
+# new_weight_input_hidden = weight_input_hidden + delta_weight_input_hidden
+# print(new_weight_input_hidden)
 
-error_input = np.matmul(weight_input_hidden.T, error_hidden)
-# print(error_input)
 
-delta_weight_input_hidden = np.matmul(LEARNING_RATE*(-2)*error_hidden*(ut.sigmoid_function(input_hidden))*(1-ut.sigmoid_function(input_hidden)), input_hidden.T)
-print(delta_weight_input_hidden)
 
-new_weight_input_hidden = weight_input_hidden + delta_weight_input_hidden
-print(new_weight_input_hidden)
